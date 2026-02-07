8cfc5bb45fc1921bd033187132f97a2fc1c65bdd
luis-dm <lfdemesa1@up.edu.ph>
2026-02-07 20:26:25 +0800

    fix import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 // app/layout.tsx
 import "./globals.css";
 import Header from "./Header";
-import { Providers } from "./providers";
+import Providers from "./providers";
 import { Toaster } from "react-hot-toast";
 
 export const metadata = {
