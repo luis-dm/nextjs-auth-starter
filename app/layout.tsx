67653782c4a4e1289278b2f9715b903d7a0414dc
luis-dm <lfdemesa1@up.edu.ph>
2026-03-30 02:01:59 +0800

    dark stop

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
